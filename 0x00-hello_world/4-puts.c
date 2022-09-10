6166564b7069bda460d000a0b2f019a0af82f699
yettyola <yettyolukemi@gmail.com>
2022-09-09 22:13:33 -0700

    task 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,22 @@
-#!/bin/bash
-("\"Programming is like building a multilingual puzzle")
+
+#include <stdio.h>#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
+
+/**
+ * main - print using put
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	"Programming is like breturn (0);
