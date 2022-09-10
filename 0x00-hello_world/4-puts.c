de5f62b31eb5b5581eea27e65b64d5a205597b9f
yettyola <yettyolukemi@gmail.com>
2022-09-10 11:43:50 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
+
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print using put
+ *
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-         puts( "\"Programming is like building a multilingual puzzle");
-	 return (0);
-}
+	"Programming is like breturn (0);
