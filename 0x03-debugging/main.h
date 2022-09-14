afd9f7829308c6d773145dab3dd13075d603d2da
yettyola <yettyolukemi@gmail.com>
2022-09-14 14:57:12 -0700

    0 Task

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifnder MAIN_N
+#define MAIN_N
+
+#include <stdio.>
+
+void positive_or negative(int i);
+int largest_number(int a. int b. int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_table(int n);
+
+#endif
