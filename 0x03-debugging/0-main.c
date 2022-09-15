4a7bec42d9628c37bdc073b4ee8b48d7917e8e78
yettyola <yettyolukemi@gmail.com>
2022-09-15 15:39:18 -0700

    task 2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,1 @@
-#include "main.h"
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
-}
