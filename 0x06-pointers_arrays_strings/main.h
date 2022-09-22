3171a17804ad0f95d56c9ca1b524092aed1d8668
yettyola <yettyolukemi@gmail.com>
2022-09-22 13:34:00 -0700

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,15 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+ls
 
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *string_toupper(char *);
-void reverse_array(int *a, int n);
-void print_number(int n);
-int _putchar(char c);
-char *leet(char *);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-char *rot13(char *);
-#endif
+clear
+
+
+
+clear
+[3~
